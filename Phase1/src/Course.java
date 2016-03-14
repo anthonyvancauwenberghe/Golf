19b611835b885194c172ff3cf14ff98a3cee6895
Nibbla <Niebisch.markus@gmx.de>
2016-03-14 17:13:58 +0100

    Type and Tile basically implemented Course added definition

--- Phase1/src/Course.java
+++ Phase1/src/Course.java
@@ -1,5 +1,12 @@
 /**
  * Created by nibbla on 14.03.16.
  */
+
+/** A course is defined by a 3 dimensional grid containing of tiles
+ * A typical course would be 800*600*100 tiles, mostly empty
+ * A tile is 1cm³
+ *
+ */
 public class Course {
+
 }
--- Phase1/src/Tile.java
+++ Phase1/src/Tile.java
@@ -1,5 +1,16 @@
+import java.awt.*;
+
 /**
  * Created by nibbla on 14.03.16.
  */
 public class Tile {
+    Type t;
+
+    double getFriction(){
+        return t.getFriction();
+    }
+    Color getColor(){
+        return t.getColor();
+    }
+
 }
