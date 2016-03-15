e80e63d2b0c17fdfdc2f219f0b8c09ffa48543c4
ejkemper <esther-kemper@live.nl>
2016-03-15 18:06:55 +0100

    updated GUI

--- Phase1/src/GUIPackage/ClickListener.java
+++ Phase1/src/GUIPackage/ClickListener.java
@@ -6,8 +6,6 @@ import java.awt.event.ActionListener;
 
 public class ClickListener implements ActionListener
 {
-    public  int times = 0;
-
     public void actionPerformed(ActionEvent event)
     {
         GUI gui = new GUI();
