0ad3320f3c4d1092bda924d4957ca8854421bbbf
Nibbla <Niebisch.markus@gmx.de>
2016-03-16 16:20:28 +0100

    hole

---
+++ Phase1/src/Game/Hole.java
@@ -0,0 +1,40 @@
+package Game;
+
+/**
+ * Created by nibbla on 16.03.16.
+ */
+public class Hole extends Tile {
+    double radius = 4;
+    public Hole(double radius, int x, int y, int z) {
+        super(Type.Hole, x, y, z);
+        this.radius = radius;
+    }
+
+    boolean isBallInHole(Ball b){
+        Coordinate bc = b.getCoordinate();
+        if (Math.abs(z-bc.getZ())>b.radius){
+            return false;
+        }
+        if (Coordinate.getDistance(x, y,z, bc.getX(), bc.getY(), bc.getZ()) < (radius-b.radius)) {
+            if (b.speedX+b.speedY >= Config.maxSpeedToFallIntoHole) return false;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param b
+     * @return true if the center of the ball is within the radius of the hole
+     */
+    boolean isBallIntersectingHole(Ball b){
+        Coordinate bc = b.getCoordinate();
+        if (Coordinate.getDistance(x, y,z, bc.getX(), bc.getY(), bc.getZ()) < (radius)) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+}
