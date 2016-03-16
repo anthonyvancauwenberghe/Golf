8915990993a70d74dce7a36e6e09cceb2cc8ebc5
Nibbla <Niebisch.markus@gmx.de>
2016-03-16 15:41:14 +0100

    Coordinate uses double

---
+++ Phase1/src/Game/Config.java
@@ -0,0 +1,9 @@
+package Game;
+
+/** All the variables are static and used to determine the game flow
+ * Created by nibbla on 16.03.16.
+ */
+public class Config {
+    public static final double maxSpeedToFallIntoHole = 3;
+
+}
