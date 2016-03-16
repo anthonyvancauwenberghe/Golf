7ddd1d4694b409aea5ba6509fe69088b1c1ff1b6
Nibbla <Niebisch.markus@gmx.de>
2016-03-16 18:31:55 +0100

    Every

--- Phase1/src/Game/Game.java
+++ Phase1/src/Game/Game.java
@@ -6,8 +6,7 @@ public class Game {
     ArrayList<Player> players;
     int currentPlayer;
     Course c;
-    ArrayList<Ball> balls;
-    int currentBall;
+
     Long time;
     private boolean gameFinished = false;
 
@@ -20,34 +19,10 @@ public class Game {
     public Game(Course course, ArrayList<Player> players) {
         this.players = players;
         currentPlayer = 0;
-        balls = new ArrayList<>(players.size());
-        for (int i = 0; i < players.size(); i++) {
-            //balls.add(new Ball());
-        }
-        currentBall = 0;
         time = 0L;
     }
 
-    public void shoot(double angX, double angY, double power){
-        
-        //TODO how the physics gets implemented.
-        time++; //basic idea of this method. needs to be adjusted.
-        //balls.get(currentBall).magic(c, angX, angY, power);
-        players.get(currentPlayer).addStroke();
 
-        //checks if the ball is still in Play and switches players according to that.
-        if (!balls.get(currentBall).inPlay()){ //ball is in the whole
-            players.get(currentPlayer).resetCurrentStrokes();
-            balls.remove(currentBall);
-            currentPlayer = (currentPlayer) % players.size();
-            currentBall = (currentBall) % balls.size();
-
-            if (balls.size()==0)gameFinished=true;
-        }else {
-            currentPlayer = (currentPlayer + 1) % players.size();
-            currentBall = (currentBall + 1) % balls.size();
-        }
-    }
     public static void main(String[] args) {
         System.out.println("golf game: whazzaa");
     }
@@ -62,14 +37,6 @@ public class Game {
 
     public Course getCourse() {
         return c;
-    }
-
-    public ArrayList<Ball> getBalls() {
-        return balls;
-    }
-
-    public Ball getCurrentBall() {
-        return balls.get(currentBall);
     }
 
     public Long getTime() {
