2603d18173d87352aa2c597a71f58625db1406c3
herredro <lfYago18>
2016-03-17 16:08:25 +0100

    GUI

--- Phase1/src/GUIPackage/GUIEditor.java
+++ Phase1/src/GUIPackage/GUIEditor.java
@@ -3,6 +3,7 @@ package GUIPackage;
 
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,6 +14,7 @@ public class GUIEditor {
     private JPanel mainPane;
     private JButton EXITButton;
     private JPanel rightPane;
+    private JPanel testPane;
     private JTextArea EDITORTextArea;
     private JComboBox comboBox1;
     private JTextPane welcomeTextPane;
@@ -20,6 +22,8 @@ public class GUIEditor {
     private JButton butBackMain;
 
     public GUIEditor() {
+
+
 
 
         butBackMain.addActionListener(new ActionListener() {
@@ -45,4 +49,21 @@ public class GUIEditor {
         return frame;
     }
 
+    public JPanel getComboPanel(int MAX_COMBOS){
+
+        String[] options = {"0","1","2","3","4","5","6","7","8","9","10"};
+        JPanel comboPanel = new JPanel(new GridLayout(10, 10));
+        JComboBox[] combos = new JComboBox[MAX_COMBOS]; // MAX_... is a constant
+        for (int i = 0; i < combos.length; i++) {
+            combos[i] = new JComboBox(options);
+            //combos[i].addActionListener(someActionListener);
+            comboPanel.add(combos[i]);
+        }
+        return comboPanel;
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        testPane = getComboPanel(100);
+    }
 }
