f453517dfe0149f8ff817d2bdf13b7ec49277370
herredro <lfYago18>
2016-06-08 14:36:08 +0200

    NormalDistribution Testing

---
+++ Phase1/src/Testings/TestNormalDistribution.java
@@ -0,0 +1,17 @@
+package Testings;
+
+import org.apache.commons.math3.distribution.NormalDistribution;
+public class TestNormalDistribution {
+    public static void main(String[]args){
+        NormalDistribution normal = new NormalDistribution(1.00, 0.20);
+        System.out.println(normal.sample());
+        System.out.println(normal.sample());
+        System.out.println(normal.sample());
+        System.out.println(normal.sample());
+        System.out.println(normal.sample());
+        System.out.println(normal.sample());
+        System.out.println(normal.sample());
+        System.out.println(normal.sample());
+        System.out.println(normal.sample());
+    }
+}
