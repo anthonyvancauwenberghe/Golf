f60750552633da1d9b9f79dd6d606b70514d339e
deviouspk <aamining2@gmail.com>
2016-06-21 16:56:07 +0200

    integration pathfinding

--- Phase1/src/Game/Model/Coordinate.java
+++ Phase1/src/Game/Model/Coordinate.java
@@ -277,6 +277,32 @@ public class Coordinate {
         }
         return passedPoints;
     }
+    public static ArrayList<int[]> getPixelBetween(int x1, int y1, int x2, int y2){
+        ArrayList<int[]> passedPoints = new ArrayList<>();
+        int dx = Math.abs(x2 - x1);
+        int dy = Math.abs(y2 - y1);
+        int sx = (x1 < x2) ? 1 : -1;
+        int sy = (y1 < y2) ? 1 : -1;
+        int err = dx - dy;
+        while (true) {
+            int[]temp = {x1, y1};
+            passedPoints.add(temp);
+            if (x1 == x2 && y1 == y2) {
+                break;
+            }
+            int e2 = 2 * err;
+            if (e2 > -dy) {
+                err = err - dy;
+                x1 = x1 + sx;
+            }
+            if (e2 < dx) {
+                err = err + dx;
+                y1 = y1 + sy;
+            }
+        }
+        return passedPoints;
+    }
+
 
     public static double getDistance(Coordinate coordinate, Coordinate coordinate1) {
         return getDistance(coordinate.xCoord,coordinate.yCoord,coordinate.zCoord,coordinate1.xCoord,coordinate1.yCoord,coordinate1.zCoord);
